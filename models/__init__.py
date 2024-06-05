575efbaf394c04cf9749715368aceee3b6f98ecd
Kenechukwu Akubue <kengoon19@gmail.com>
2024-06-05 14:47:48 +0100

    ../models/

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""This module instantiates an instance of the Storage will be used"""
+"""
+initialize the models package
+"""
 
 from os import getenv
-from config import HBNB_TYPE_STORAGE
 
-if HBNB_TYPE_STORAGE == 'db':
+
+storage_t = getenv("HBNB_TYPE_STORAGE")
+
+if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-
 storage.reload()
